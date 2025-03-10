51a44e8b187629e4493bb40577c2d15fcf7601eb
Dawam Raja <akunnyadawam9@gmail.com>
2025-03-10 15:56:46 +0700

    ✨ (src/extension.js): initial commit of the extension with TypeScript files and a command to add cursor rules

---
+++ src/extension.ts
@@ -0,0 +1,14 @@
+import * as vscode from 'vscode';
+import { addCursorRuleCommand } from './cmd/add';
+import { Cache } from './utils/cache';
+
+export function activate(context: vscode.ExtensionContext) {
+  Cache.getInstance(context);
+
+  let disposable = vscode.commands.registerCommand('cursorRules.addRule', () => {
+    return addCursorRuleCommand(context);
+  });
+  context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
